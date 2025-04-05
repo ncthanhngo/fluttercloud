39a8f1664529e39698283f2946acbc6057dde61f
Thanh Ngo <nc.thanhngo@gmail.com>
2025-04-05 15:39:58 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:fluttercloud/app/bt01.dart';
+
+void main(List<String> args) {
+  runApp(Bt01());
+}
---
+++ lib/pages/bt01_home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Bt01HomePage extends StatelessWidget {
+  const Bt01HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
