7a603692e3b2cf0f670bd6a840976acd1118b88d
Thanh Ngo <nc.thanhngo@gmail.com>
2025-04-06 23:39:19 +0700

    Update bt_01.dart

--- lib/app/bt_01.dart
+++ lib/app/bt_01.dart
@@ -4,6 +4,7 @@ import 'package:fluttercloud/pages/bt_01_page.dart';
 class Bt01 extends StatelessWidget {
   const Bt01({super.key});
 
+  //Them vao
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(home: Bt01Page());
